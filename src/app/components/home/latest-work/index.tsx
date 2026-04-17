44fd22eba7a3375b6ac46d652ded481b3ec138da
Mustafa Aslanov <aslanov7509@gmail.com>
2026-04-18 01:07:47 +0400

    Portfolio is updated

--- src/app/components/home/latest-work/index.tsx
+++ src/app/components/home/latest-work/index.tsx
@@ -29,7 +29,7 @@ const LatestWork = () => {
           <div className="py-16 xl:py-32 ">
             <div className="flex items-center justify-between gap-2 border-b border-black pb-7 mb-9 md:mb-16">
               <h2>Latest Works</h2>
-              <p className="text-xl text-orange-500">( 04 )</p>
+              <p className="text-xl text-orange-500">( 02 )</p>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-6 gap-y-6 xl:gap-y-12">
               {workData?.map((value: any, index: any) => {
